ff5244287329d978519fe07fab04c3d1662e1cf2
wangyuhnag <wangyuhang@xinzhengkeji.com>
2021-02-02 18:43:17 +0800

    三方埋点

---
+++ AdObj/app/src/main/java/com/idxz/adobj/EventObj.kt
@@ -0,0 +1,46 @@
+package com.idxz.adobj
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import com.appsflyer.AppsFlyerLib
+import com.facebook.appevents.AppEventsConstants
+import com.facebook.appevents.AppEventsLogger
+import com.google.firebase.analytics.FirebaseAnalytics
+import io.branch.referral.util.BranchEvent
+import java.util.HashMap
+
+object EventObj {
+
+    @SuppressLint("MissingPermission")
+    fun setEvent(
+        content: Context,
+        customerId: String,
+        branchEventName: String,
+        firebaseEventName: String,
+        faceBookEventName:String,
+        appsFlyerEventName:String
+    ) {
+
+        val logger = AppEventsLogger.newLogger(content)
+        val firebaseAnalytics = FirebaseAnalytics.getInstance(content)
+
+        val b = Bundle()
+        b.putString("customer_id", customerId)
+
+        AppsFlyerLib.getInstance().setCustomerUserId(customerId)
+        BranchEvent(branchEventName)
+            .addCustomDataProperty("customer_id", customerId)
+            .logEvent(content)
+
+        b.putLong("click_time", System.currentTimeMillis())
+        firebaseAnalytics.logEvent(firebaseEventName, b)
+        logger.logEvent(faceBookEventName, b)
+
+        val mapData = HashMap<String, Any>()
+        mapData.put(appsFlyerEventName, System.currentTimeMillis())
+        AppsFlyerLib.getInstance()
+            .trackEvent(content, appsFlyerEventName, mapData)
+    }
+
+}
